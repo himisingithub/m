2e6175358b545b0dfddeac06f60ee22fc9ed43b9
himisingithub <157177745+himisingithub@users.noreply.github.com>
2024-01-23 21:57:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["𝗢𝗙𝗙𝗖𝗜𝗔𝗟 𝗜𝗦𝗙 𝗧𝗜𝗖𝗞𝗘𝗧 𝗕𝗢𝗧"];
 
 
 let currentIndex = 0;
